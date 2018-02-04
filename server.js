4b10d60a9a8c27768691c74f935a753abc9cc310
Brian Elliston <brianelliston@Brians-MacBook-Pro.local>
2018-02-04 11:05:28 -0500

    Added some styling and a button

--- server.js
+++ server.js
@@ -1,182 +1,79 @@
-// Dependencies
-var express = require("express");
-var mongojs = require("mongojs");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
 
+
+var express = require("express");
+var exphbs = require("express-handlebars");
+var goose = require("mongoose");
+var body = require("body-parser");
+var cheerio = require("cheerio");
+var request = require("request");
+var mongojs = require("mongojs");
+
+// Initialize Express
 var app = express();
 
-// Set the app up with morgan.
-// morgan is used to log our HTTP Requests. By setting morgan to 'dev' 
-// the :status token will be colored red for server error codes, 
-// yellow for client error codes, cyan for redirection codes, 
-// and uncolored for all other codes.
-app.use(logger("dev"));
-// Setup the app with body-parser and a static folder
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
-app.use(express.static("public"));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 // Database configuration
-var databaseUrl = "notetaker";
-var collections = ["notes"];
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
 
-// Hook mongojs config to db variable
+
+
+
+
+// Main route (simple Hello World Message)
+app.get("/", function (req, res) {
+    res.render("index");
+  });
+
+// Hook mongojs configuration to the db variable
 var db = mongojs(databaseUrl, collections);
-
-// Log any mongojs errors to console
-db.on("error", function(error) {
+db.on("error", function (error) {
   console.log("Database Error:", error);
 });
 
-// Routes
-// ======
+// This route will retrieve all of the data
+// from the scrapedData collection as a json (this will be populated
+// by the data you scrape using the next route)
+app.get("/scrape", function (req, res) {
+    console.log("hello world")
+    request("https://www.wncmagazine.com/", function (error, response, html) {
+      var $ = cheerio.load(html);
+      $(".field-content").each(function(i, element) {
+        
+            var link = $(element).children().attr("href");
+            var title = $(element).children().text();
+            var summary = $(element).parent().find("div.field-content").text();
+            // Save these results in an object that we'll push into the results array we defined earlier
+            
+        if (title && link) {
+          db.scrapeData.insert({
+            title: title,
+            link: link
+            
+          },
+          function(err, inserted){
+  
+            if(err){
+              console.log(err);
+            }
+            else{
+              console.log(inserted);
+            }
+          });
+        }
+      });
+    });
+  
+  })
 
-// Simple index route
-app.get("/", function(req, res) {
-  res.send(index.html);
-});
-
-// Handle form submission, save submission to mongo
-app.post("/submit", function(req, res) {
-  console.log(req.body);
-  // Insert the note into the notes collection
-  db.notes.insert(req.body, function(error, saved) {
-    // Log any errors
-    if (error) {
-      console.log(error);
-    }
-    else {
-      // Otherwise, send the note back to the browser
-      // This will fire off the success function of the ajax request
-      res.send(saved);
-    }
-  });
-});
-
-// Retrieve results from mongo
-app.get("/all", function(req, res) {
-  // Find all notes in the notes collection
-  db.notes.find({}, function(error, found) {
-    // Log any errors
-    if (error) {
-      console.log(error);
-    }
-    else {
-      // Otherwise, send json of the notes back to user
-      // This will fire off the success function of the ajax request
-      res.json(found);
-    }
-  });
-});
-
-// Select just one note by an id
-app.get("/find/:id", function(req, res) {
-  // When searching by an id, the id needs to be passed in
-  // as (mongojs.ObjectId(IDYOUWANTTOFIND))
-
-  // Find just one result in the notes collection
-  db.notes.findOne(
-    {
-      // Using the id in the url
-      _id: mongojs.ObjectId(req.params.id)
-    },
-    function(error, found) {
-      // log any errors
-      if (error) {
-        console.log(error);
-        res.send(error);
-      }
-      else {
-        // Otherwise, send the note to the browser
-        // This will fire off the success function of the ajax request
-        console.log(found);
-        res.send(found);
-      }
-    }
-  );
-});
-
-// Update just one note by an id
-app.post("/update/:id", function(req, res) {
-  // When searching by an id, the id needs to be passed in
-  // as (mongojs.ObjectId(IDYOUWANTTOFIND))
-
-  // Update the note that matches the object id
-  db.notes.update(
-    {
-      _id: mongojs.ObjectId(req.params.id)
-    },
-    {
-      // Set the title, note and modified parameters
-      // sent in the req's body.
-      $set: {
-        title: req.body.title,
-        note: req.body.note,
-        modified: Date.now()
-      }
-    },
-    function(error, edited) {
-      // Log any errors from mongojs
-      if (error) {
-        console.log(error);
-        res.send(error);
-      }
-      else {
-        // Otherwise, send the mongojs response to the browser
-        // This will fire off the success function of the ajax request
-        console.log(edited);
-        res.send(edited);
-      }
-    }
-  );
-});
-
-// Delete One from the DB
-app.get("/delete/:id", function(req, res) {
-  // Remove a note using the objectID
-  db.notes.remove(
-    {
-      _id: mongojs.ObjectID(req.params.id)
-    },
-    function(error, removed) {
-      // Log any errors from mongojs
-      if (error) {
-        console.log(error);
-        res.send(error);
-      }
-      else {
-        // Otherwise, send the mongojs response to the browser
-        // This will fire off the success function of the ajax request
-        console.log(removed);
-        res.send(removed);
-      }
-    }
-  );
-});
-
-// Clear the DB
-app.get("/clearall", function(req, res) {
-  // Remove every note from the notes collection
-  db.notes.remove({}, function(error, response) {
-    // Log any errors to the console
-    if (error) {
-      console.log(error);
-      res.send(error);
-    }
-    else {
-      // Otherwise, send the mongojs response to the browser
-      // This will fire off the success function of the ajax request
-      console.log(response);
-      res.send(response);
-    }
-  });
-});
 
 // Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
-});
+app.listen(3000, function () {
+    console.log("App running on port 3000!");
+  });
