8acbb8a63ed2dfa92e8177b8ccd1185c6a2082cd
Brian Elliston <brianelliston@Brians-MacBook-Pro.local>
2018-02-07 09:23:14 -0500

    Added models folder and broke everything

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-
-
 var express = require("express");
 var exphbs = require("express-handlebars");
-var goose = require("mongoose");
-var body = require("body-parser");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 var cheerio = require("cheerio");
 var request = require("request");
 var mongojs = require("mongojs");
-
+var logger = require("morgan");
+var axios = require("axios");
+var db = require("./models")
 // Initialize Express
 var app = express();
 
@@ -20,22 +20,31 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 
 // Database configuration
 var databaseUrl = "scraper";
-var collections = ["scrapedData"];
+var collections = ["scrapeData"];
 
 
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: false }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
 
 
-
-// Main route (simple Hello World Message)
+// Main route 
 app.get("/", function (req, res) {
-    res.render("index");
+  // db.Article.find({})
+  // .then(function(art){
+  //   res.render("index", {articles: art});
+  // })
+  // .catch(function(err) {
+  //   // If an error occurs, send the error back to the client
+  //   res.json(err);
+  // });
+   res.send("Hello THere")
   });
 
-// Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
-db.on("error", function (error) {
-  console.log("Database Error:", error);
-});
+
 
 // This route will retrieve all of the data
 // from the scrapedData collection as a json (this will be populated
@@ -50,12 +59,12 @@ app.get("/scrape", function (req, res) {
             var title = $(element).children().text();
             var summary = $(element).parent().find("div.field-content").text();
             // Save these results in an object that we'll push into the results array we defined earlier
-            
+            var result ={};
+            result.title = title;
+            result.link = link;
         if (title && link) {
-          db.scrapeData.insert({
-            title: title,
-            link: link
-            
+          db.Article.create({
+            result
           },
           function(err, inserted){
   
@@ -63,11 +72,15 @@ app.get("/scrape", function (req, res) {
               console.log(err);
             }
             else{
-              console.log(inserted);
+              console.log(inserted); 
+              // res.render("index", {articles: inserted});
+              res.send("Scrape finished")
             }
+          
           });
         }
       });
+      
     });
   
   })
